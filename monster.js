7832d95dedf182b67af61b10884590de13cdca39
fi67 <fi67@users.noreply.github.com>
2019-12-04 10:02:48 +0000

    Add files via upload

---
+++ monster.js
@@ -0,0 +1,21 @@
+// Code to set things up
+let randomNumber;
+let myDur;
+let myPos;
+const root = document.documentElement;
+const lightning = document.getElementById('lightning');
+const head = document.getElementById('head');
+const eyes = document.getElementById('eyes');
+const nose = document.getElementById('nose');
+const mouth = document.getElementById('mouth');
+const monsterButton = document.getElementById('monsterButton');
+monsterButton.addEventListener("click",makeMe);
+
+//Random lightning
+
+
+
+function makeMe() {
+ // code that runs when the button is clicked
+    
+}
